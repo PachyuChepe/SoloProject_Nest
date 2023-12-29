7d4a996ec59a8f92595cf2b6de7882d79ac638c7
PachyuChepe <pachyuchepe@gmail.com>
2023-12-29 16:50:51 +0900

    Feat: 좌석 템플릿 & 템플릿 기반 생성 시 관리자 권한 유무 확인 로직 추가

--- src/seat-template/seat-template.controller.ts
+++ src/seat-template/seat-template.controller.ts
@@ -1,16 +1,31 @@
 // src/seat-template/seat-template.controller.ts
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UseGuards,
+  Req,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { SeatTemplateService } from './seat-template.service';
 import { CreateSeatTemplateDto } from './dto/create-seat-template.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
 
 @Controller('seat-templates')
 export class SeatTemplateController {
   constructor(private seatTemplateService: SeatTemplateService) {}
 
   @Post()
+  @UseGuards(AuthGuard('jwt'), JwtAuthGuard)
   async createSeatTemplate(
     @Body() createSeatTemplateDto: CreateSeatTemplateDto,
+    @Req() req,
   ) {
+    if (!req.user.isAdmin) {
+      throw new UnauthorizedException('관리자 권한이 필요합니다.');
+    }
+
     return this.seatTemplateService.createSeatTemplate(createSeatTemplateDto);
   }
 
--- src/seat/seat.controller.ts
+++ src/seat/seat.controller.ts
@@ -1,17 +1,32 @@
 // src/seat/seat.controller.ts
 
-import { Controller, Post, Param } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Param,
+  UseGuards,
+  UnauthorizedException,
+  Req,
+} from '@nestjs/common';
 import { SeatService } from './seat.service';
+import { AuthGuard } from '@nestjs/passport';
+import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
 
 @Controller('seats')
 export class SeatController {
   constructor(private seatService: SeatService) {}
 
   @Post('/create-from-template/performance/:performanceId/template/:templateId')
+  @UseGuards(AuthGuard('jwt'), JwtAuthGuard)
   async createSeatsFromTemplate(
     @Param('performanceId') performanceId: number,
     @Param('templateId') templateId: number,
+    @Req() req,
   ) {
+    if (!req.user.isAdmin) {
+      throw new UnauthorizedException('관리자 권한이 필요합니다.');
+    }
+
     return this.seatService.createSeatsFromTemplate(performanceId, templateId);
   }
 
